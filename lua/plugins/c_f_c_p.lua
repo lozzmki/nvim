d9cc6a9d752c25042b0e4ac148abf6bb99300982
lozzmki <lozzmki@163.com>
2023-02-08 14:56:53 +0800

    change fzf default command to GFiles

--- lua/plugins/c_f_c_p.lua
+++ lua/plugins/c_f_c_p.lua
@@ -1,5 +1,5 @@
 local set = vim.keymap.set
 local opt = {noremap = true, silent = true}
-set('i', '<C-p>', ":Files<CR>", opt)
-set('n', '<C-p>', ":Files<CR>", opt)
-set('v', '<C-p>', ":Files<CR>", opt)
+set('i', '<C-p>', ":GFiles<CR>", opt)
+set('n', '<C-p>', ":GFiles<CR>", opt)
+set('v', '<C-p>', ":GFiles<CR>", opt)
